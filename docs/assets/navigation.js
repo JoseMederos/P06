40cc21f48df360243fbf3401e2c2d9de996b3a6c
Jose Mederos <alu0101368832@ull.edu.es>
2025-03-12 11:17:49 +0000

    final push

--- docs/assets/navigation.js
+++ docs/assets/navigation.js
@@ -1,1 +1,1 @@
-window.navigationData = "eJyLrlYqSa0oUbJSSkxJUdJRKkgsyVCyUkorzUsuyczPK9ZPTEnRyyjJzVHSUcrOzEtRsjIzqdWBa0rJLMOmKSWzDI+m4tIkbJqKS5MwNMUCAJBWNLo="
+window.navigationData = "eJx9zsEKwjAQBNB/mXNQLCglN+ndHxAPabqSxU1SkhUK0n/3poSi5xnezPUFpUVhcS6sIZKyd6PQkEXIK+cEg9lpgIUXVyvV/a/mLmgUGDw4TbCHrl/NRx9ynIWWyzOOVLZkE/9z2u0vxEmp3J3f3Gux7nhab29nTlVq"
--- tests/mod.spec.ts
+++ tests/mod.spec.ts
@@ -1,35 +1,78 @@
 import { describe, expect, test } from "vitest";
-import { ComplexNumber } from "../src/mod.ts";
+import { ComplexNumber, ArithmeticableCollection } from "../src/mod.ts";
 
 const complexNumber1 = new ComplexNumber(1, 3);
 const complexNumber2 = new ComplexNumber(2, 6);
-const complexNumber12Add = new ComplexNumber(3, 9);
-const complexNumber12Sub = new ComplexNumber(-1, -3);
-const complexNumber12Mult = new ComplexNumber(-16, 0);
-const complexNumber12Div = new ComplexNumber(-1, -3);
 
+const complexNumber3 = new ComplexNumber(-2, 3);
+const complexNumber4 = new ComplexNumber(2, -1);
+const complexNumber5 = new ComplexNumber(1, 1);
 
 describe("add function tests", () => {
+  test("add(1 + 3i, 2 + 6i) returns value 3 + 9i", () => {
+    expect(complexNumber1.add(complexNumber2)).toStrictEqual(
+      new ComplexNumber(3, 9),
+    );
+  });
 
-  test("add(1 + 3i, 2 + 6i) returns value 9", () => {
-    expect(complexNumber1.add(complexNumber2)).toStrictEqual(complexNumber12Add);
+  test("add(-2 + 3i, 2 -1i) returns value 0 + 2i", () => {
+    expect(complexNumber3.add(complexNumber4)).toStrictEqual(
+      new ComplexNumber(0, 2),
+    );
   });
 });
 
 describe("substract function tests", () => {
-  test("substract(1 + 3i, 2 + 6i) returns value 3", () => {
-    expect(complexNumber1.substract(complexNumber2)).toStrictEqual(complexNumber12Sub);
+  test("substract(1 + 3i, 2 + 6i) returns value -1 - 3i", () => {
+    expect(complexNumber1.substract(complexNumber2)).toStrictEqual(
+      new ComplexNumber(-1, -3),
+    );
+  });
+
+  test("substract(-2 + 3i, 2 -1i) returns value -4 + 4i", () => {
+    expect(complexNumber3.substract(complexNumber4)).toStrictEqual(
+      new ComplexNumber(-4, 4),
+    );
   });
 });
 
 describe("miltiply function tests", () => {
-  test("multiply(1 + 3i, 2 + 6i) throws an error", () => {
-    expect(complexNumber1.multiply(complexNumber2)).toStrictEqual(complexNumber12Mult);
+  test("multiply(1 + 3i, 2 + 6i) returns value -16", () => {
+    expect(complexNumber1.multiply(complexNumber2)).toStrictEqual(
+      new ComplexNumber(-16, 0),
+    );
+  });
+
+  test("multiply(-2 + 3i, 2 -1i) returns value -1 + -4i", () => {
+    expect(complexNumber3.multiply(complexNumber4)).toStrictEqual(
+      new ComplexNumber(-1, -4),
+    );
   });
 });
 
 describe("divide function tests", () => {
-  test("divide(1 + 3i, 2 + 6i) throws an error", () => {
-    expect(complexNumber1.substract(complexNumber2)).toStrictEqual(complexNumber12Div);
+  test("divide(1 + 3i, 2 + 6i) return vakue -1 - 3i", () => {
+    expect(complexNumber1.substract(complexNumber2)).toStrictEqual(
+      new ComplexNumber(-1, -3),
+    );
+  });
+
+  test("divide(1 + 1i, -2 + 3i) returns value 10 + 4i", () => {
+    expect(complexNumber5.divide(complexNumber3)).toStrictEqual(
+      new ComplexNumber(10, 4),
+    );
+  });
+});
+
+describe("ArithmeticableCollection tests", () => {
+  const collection = new ArithmeticableCollection();
+  collection.addArithmeticable(complexNumber1);
+  test("addArithmeticable(complexNumber1) and getArithmeticable(0)", () => {
+    expect(collection.getArithmeticable(0)).toStrictEqual(complexNumber1);
+  });
+
+  collection.addArithmeticable(complexNumber1);
+  test("getNumberOfArithmeticables", () => {
+    expect(collection.getNumberOfArithmeticables()).toStrictEqual(2);
   });
 });
